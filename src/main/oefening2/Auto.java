e9afd35aa5e5574dc743b941cd513f9fb72a0bec
Lars van Vliet <123321@student.drenthecollege.nl>
2019-03-17 21:05:53 +0100

    Alle getters en setters gemaakt voor Auto en dat wordt in Main gebruikt

--- src/main/oefening2/Auto.java
+++ src/main/oefening2/Auto.java
@@ -3,6 +3,7 @@ package src.main.oefening2;
 public class Auto extends Vervoermiddel {
     private int gewicht;
     private double belasting;
+    private String brandstof;
 
     public void setGewicht(int gewicht) { this.gewicht = gewicht; }
     public int getGewicht() { return this.gewicht; }
@@ -12,5 +13,36 @@ public class Auto extends Vervoermiddel {
 
     @Override
     public int getAantalWielen() { return 4; }
+
+    public void setBrandstof(String brandstof) { this.brandstof = brandstof; }
+    public String getBrandstof() { return this.brandstof; }
+
+    public double getWegenbelasting() {
+        switch(this.brandstof) {
+            case "benzine":
+                if (this.gewicht <= 1000) {
+                    return 241.45;
+                } else {
+                    return 584.89;
+                }
+
+            case "diesel":
+                if (this.gewicht <= 900) {
+                    return 182.24;
+                } else {
+                    return 499.99;
+                }
+
+            case "gas":
+                if (this.gewicht <= 1100) {
+                    return 309.83;
+                } else {
+                    return 629.92;
+                }
+
+            default:
+                return 0.00;
+        }
+    }
 
 }
--- src/main/oefening2/Main.java
+++ src/main/oefening2/Main.java
@@ -3,6 +3,13 @@ package src.main.oefening2;
 public class Main {
 
     public static void main(String[] args) {
+        Auto auto1 = new Auto();
+        auto1.setMerk("Renault");
+        auto1.setType("N4TZ");
+        auto1.setKleur("zwart");
+        auto1.setBrandstof("gas");
+        auto1.setGewicht(947);
 
+        System.out.println("Wegenbelasting is: " + auto1.getWegenbelasting());
     }
 }
