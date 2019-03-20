bf05be88a4cad9b659a8fadf66e02d6779fdf5e7
Lars van Vliet <123321@student.drenthecollege.nl>
2019-03-20 13:12:31 +0100

    indentation gewijzigd

--- src/main/oefening4/model/PersonModel.java
+++ src/main/oefening4/model/PersonModel.java
@@ -8,10 +8,12 @@ public class PersonModel {
     private List<Person> persons = new ArrayList<>();
 
     public void addPerson(Person person) { // add person to list of persons
+
         persons.add(person);
     }
 
     public List<Person> getPersons() { // get the list of persons
+
         return persons;
     }
 }
