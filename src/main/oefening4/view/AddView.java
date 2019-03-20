bb9e06b3a161dbe34d1330ce2cf4a596724b2303
Lars van Vliet <123321@student.drenthecollege.nl>
2019-03-20 13:13:21 +0100

    clearField methode en commentaar toegevoegd

--- src/main/oefening4/view/AddView.java
+++ src/main/oefening4/view/AddView.java
@@ -12,27 +12,41 @@ public class AddView extends JFrame{
     private JPanel rootPanelSubmit;
 
     public AddView() {
-        add(rootPanelSubmit);
+        add(rootPanelSubmit); // add rootpanel
 
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // close when tab is closed instead of hiding tab
 
-        setTitle("Persoon toevoegen");
-        setSize(300, 140);
+        setTitle("Persoon toevoegen"); // set title
+        setSize(300, 140); // set size
     }
 
-    public void addSubmitButtonListener(ActionListener listenForSubmitButton) {
+    public void addSubmitButtonListener(ActionListener listenForSubmitButton) { // action listener for submit button
         submitButton.addActionListener(listenForSubmitButton);
     }
+
+    public void clearFields() { // clear the fields
+        setFirstnameField("");
+        setLastnameField("");
+        setBirthdateField("");
+    }
+
+    /* SETTERS
+                 &
+                      GETTERS */
 
     public String getFirstnameField() {
         return firstnameField.getText();
     }
+    public void setFirstnameField(String text) { firstnameField.setText(text);}
 
     public String getLastnameField() {
         return lastnameField.getText();
     }
+    public void setLastnameField(String text) { lastnameField.setText(text);}
 
     public String getBirthdateField() {
         return birthdateField.getText();
     }
+    public void setBirthdateField(String text) { birthdateField.setText(text);}
+
 }
