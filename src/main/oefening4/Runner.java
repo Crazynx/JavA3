87cf478066e338b0a86bc6f82fce38917f7c074e
Lars van Vliet <123321@student.drenthecollege.nl>
2019-03-20 09:09:40 +0100

    Runner toegevoegd om het programma te starten

--- src/main/oefening4/Runner.java
+++ src/main/oefening4/Runner.java
@@ -2,24 +2,18 @@ package src.main.oefening4;
 
 import src.main.oefening4.controller.PersoonController;
 import src.main.oefening4.model.PersoonModel;
-import src.main.oefening4.view.PersoonView;
 import src.main.oefening4.view.AddView;
-
-import javax.swing.*;
+import src.main.oefening4.view.PersoonView;
 
 public class Runner {
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         AddView addView = new AddView();
         PersoonView persoonView = new PersoonView();
         PersoonModel persoonModel = new PersoonModel();
         PersoonController persoonController = new PersoonController(addView, persoonView, persoonModel);
 
-
         persoonView.setVisible(true);
-        addView.setVisible(true);
-
-
-
     }
+
 }
