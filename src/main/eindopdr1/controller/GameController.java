3fd002d60e917e35ddb086a70fe898cdf917fdfa
Lars van Vliet <123321@student.drenthecollege.nl>
2019-03-24 14:52:47 +0100

    added action handlers

--- src/main/eindopdr1/controller/GameController.java
+++ src/main/eindopdr1/controller/GameController.java
@@ -4,6 +4,8 @@ import src.main.eindopdr1.model.GameModel;
 import src.main.eindopdr1.view.GameView;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class GameController {
 
@@ -15,8 +17,57 @@ public class GameController {
         this.gameView = gameView;
         this.gameModel = gameModel;
 
+        gameView.addButtonListener(new ButtonListener());
+        gameView.addResetListener(new ResetListener());
+
 
     }
 
+    private class ButtonListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if (e.getSource() == gameView.button1) {
+                System.out.println(e.getActionCommand());
+            } else if (e.getSource() == gameView.button2) {
+                System.out.println(e.getActionCommand());
+            } else if (e.getSource() == gameView.button3) {
+                System.out.println(e.getActionCommand());
+            } else if (e.getSource() == gameView.button4) {
+                System.out.println(e.getActionCommand());
+            } else if (e.getSource() == gameView.button5) {
+                System.out.println(e.getActionCommand());
+            } else if (e.getSource() == gameView.button6) {
+                System.out.println(e.getActionCommand());
+            } else if (e.getSource() == gameView.button7) {
+                System.out.println(e.getActionCommand());
+            } else if (e.getSource() == gameView.button8) {
+                System.out.println(e.getActionCommand());
+            } else if (e.getSource() == gameView.button9) {
+                System.out.println(e.getActionCommand());
+            } else if (e.getSource() == gameView.button10) {
+                System.out.println(e.getActionCommand());
+            } else if (e.getSource() == gameView.button11) {
+                System.out.println(e.getActionCommand());
+            } else if (e.getSource() == gameView.button12) {
+                System.out.println(e.getActionCommand());
+            } else if (e.getSource() == gameView.button13) {
+                System.out.println(e.getActionCommand());
+            } else if (e.getSource() == gameView.button14) {
+                System.out.println(e.getActionCommand());
+            } else if (e.getSource() == gameView.button15) {
+                System.out.println(e.getActionCommand());
+            } else if (e.getSource() == gameView.button16) {
+                System.out.println(e.getActionCommand());
+            }
+        }
+    }
+
+    private class ResetListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            System.out.println();
+        }
+    }
 
 }
