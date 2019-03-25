29bdcbb00986b924037c9ebe7e7901648618b9e9
Lars van Vliet <123321@student.drenthecollege.nl>
2019-03-25 09:26:58 +0100

    removed comments

--- src/main/eindopdr1/Runner.java
+++ src/main/eindopdr1/Runner.java
@@ -4,8 +4,6 @@ import src.main.eindopdr1.controller.GameController;
 import src.main.eindopdr1.model.GameModel;
 import src.main.eindopdr1.view.GameView;
 
-import javax.swing.*;
-
 public class Runner {
 
     public static void main(String[] args) {
@@ -14,17 +12,5 @@ public class Runner {
         GameController gameController = new GameController(gameModel, gameView);
 
         gameView.setVisible(true);
-
-//        JButton[][] buttons = gameView.getButtons();
-//
-//        for (int x = 0; x < buttons.length; x++) {
-//            for (int y = 0; y < buttons[x].length; y++) {
-//                System.out.println(buttons[x][y].getActionCommand());
-//            }
-//            System.out.println("--------------------------");
-//        }
     }
-
-
-
 }
--- src/main/eindopdr1/controller/GameController.java
+++ src/main/eindopdr1/controller/GameController.java
@@ -4,7 +4,6 @@ import src.main.eindopdr1.model.GameModel;
 import src.main.eindopdr1.view.GameView;
 
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
