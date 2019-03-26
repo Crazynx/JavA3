5f9c5328a0ca9ee304bc2ced491fed86cf6d14b0
Lars van Vliet <123321@student.drenthecollege.nl>
2019-03-26 13:40:12 +0100

    Simple class for testing the Database class

--- src/main/mysqlOefening/App.java
+++ src/main/mysqlOefening/App.java
@@ -1,4 +1,22 @@
 package src.main.mysqlOefening;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class App {
+
+    public static void main(String[] args) throws SQLException {
+
+        Database database = new Database();
+
+        database.getConnection();
+        database.setQuery("SELECT * FROM persons");
+        ResultSet resultSet = database.getQuery();
+
+        while(resultSet.next()) {
+            System.out.println(resultSet.getString("name"));
+        }
+
+    }
+
 }
