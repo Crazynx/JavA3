153ee4d4f8e22acfbec312f9d3f8caac8b368316
Lars van Vliet <123321@student.drenthecollege.nl>
2019-03-26 15:26:17 +0100

    removed PersonModel object and added Database object, established connection and updated the JList with values already in the database

--- src/main/eindopdr2/Runner.java
+++ src/main/eindopdr2/Runner.java
@@ -1,17 +1,20 @@
 package src.main.eindopdr2;
 
-import src.main.oefening4.controller.PersonController;
-import src.main.oefening4.model.PersonModel;
-import src.main.oefening4.view.AddView;
-import src.main.oefening4.view.PersonView;
+import src.main.eindopdr2.model.Database;
+import src.main.eindopdr2.controller.PersonController;
+import src.main.eindopdr2.view.AddView;
+import src.main.eindopdr2.view.PersonView;
 
 public class Runner {
 
     public static void main(String[] args) {
         AddView addView = new AddView();
         PersonView personView = new PersonView();
-        PersonModel personModel = new PersonModel();
-        PersonController personController = new PersonController(addView, personView, personModel);
+        Database database = new Database();
+        PersonController personController = new PersonController(addView, personView, database);
+
+        database.getConnection();
+        personView.updatePersonList(database.getPersons());
 
         personView.setVisible(true); // show the program tab
     }
